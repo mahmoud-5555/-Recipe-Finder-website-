308d9af4fd663bbfe11d4b7d92b5c4319b2582ec
Alnyle <corgoo7z49h@gmail.com>
2024-03-16 14:57:51 +0200

    complete add AddRecipe html part

---
+++ js/AddReci.js
@@ -0,0 +1,62 @@
+const ingredients = document.querySelector(".ingredients");
+
+// const ingredientID = document.querySelector('.ingredient_id');
+// const ingredientName = document.querySelector('.ingredient_name');
+// const ingredientQuantity = document.querySelector('.ingredient_quantity');
+const addBtn = document.querySelector("#add_Ingredient");
+
+console.log(ingredients.innerHTML);
+
+
+addBtn.addEventListener("click", () => {
+
+    // not working make array of objects and append the and details in it next time
+//   const ingredientElement = document.createElement("div");
+//   ingredientElement.classList.add("ingredient");
+
+//   const ingredientIDLabel = document.createElement("label");
+//   ingredientIDLabel.setAttribute("for", "ingredient_id");
+//   ingredientIDLabel.textContent = "Ingredient ID";
+//   const ingredientIDInput = document.createElement("input");
+//   ingredientIDInput.setAttribute("type", "text");
+//   ingredientIDInput.classList.add("ingredient_id");
+
+//   const ingredientNameLabel = document.createElement("label");
+//   ingredientNameLabel.setAttribute("for", "ingredient_name");
+//   ingredientNameLabel.textContent = "Ingredient Name";
+//   const ingredientNameInput = document.createElement("input");
+//   ingredientNameInput.setAttribute("type", "text");
+//   ingredientNameInput.classList.add("ingredient_name");
+
+//   const ingredientQuantityLabel = document.createElement("label");
+//   ingredientQuantityLabel.setAttribute("for", "ingredient_quantity");
+//   ingredientQuantityLabel.textContent = "Ingredient Quantity";
+//   const ingredientQuantityInput = document.createElement("input");
+//   ingredientQuantityInput.setAttribute("type", "text");
+//   ingredientQuantityInput.classList.add("ingredient_quantity");
+
+//   ingredientElement.appendChild(ingredientIDLabel);
+//   ingredientElement.appendChild(ingredientIDInput);
+//   ingredientElement.appendChild(ingredientNameLabel);
+//   ingredientElement.appendChild(ingredientNameInput);
+//   ingredientElement.appendChild(ingredientQuantityLabel);
+//   ingredientElement.appendChild(ingredientQuantityInput);
+
+    // ingredientElement.innerHTML =  `
+    
+    // <div class="ingredient">
+    // <label for="ingredient_id">Ingredient ID</label>
+    // <input type="text" class="ingredient_id">
+
+    // <label for="Ingredient_name">Ingredient Name</label>
+    // <input type="text" class="ingredient_name">
+
+    // <label for="Ingredient_quantity">Ingredient Quantity</label>
+    // <input type="text" class="ingredient_quantity">
+    // </div>
+    
+    // `
+    
+
+  ingredients.appendChild(ingredientElement);
+});
